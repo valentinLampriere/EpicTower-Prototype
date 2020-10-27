a969dbff3e7844fcfd9dd16c2b0d21c59e030b61
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-10-27 18:12:53 +0100

    created grid to snap rooms and can create snapped room on click

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    [SerializeField] private int nbCellsWidth = 0;
+    [SerializeField] private int nbCellsHeight = 0;
+    [SerializeField] private float cellsSize = 1f;
+
+    public Dictionary<Vector3, Tuple<Room, bool>> DicCells { get; private set; }
+
+    void Awake()
+    {
+        DicCells = new Dictionary<Vector3, Tuple<Room, bool>>();
+        InitGrid();
+    }
+
+    void InitGrid()
+    {
+        Vector3 gridBase = transform.position;
+        gridBase = new Vector3(Mathf.FloorToInt(gridBase.x), Mathf.FloorToInt(gridBase.y), 0);
+        transform.position = gridBase;
+
+        for (float x = gridBase.x; x < nbCellsWidth; x += cellsSize)
+        {
+            for (float y = gridBase.y; y < nbCellsHeight; y += cellsSize)
+            {
+                DicCells.Add(new Vector3((int)x, (int)y, 0), new Tuple<Room, bool>(null, false));
+            }
+        }
+    }
+
+    public Vector3 SnapToGrid(Vector3 position)
+    {
+        Vector3 snpPos = new Vector3(Mathf.FloorToInt(position.x), Mathf.FloorToInt(position.y), 0);
+
+        if(DicCells.ContainsKey(snpPos))
+        {
+            return snpPos;
+        }
+        else
+        {
+            Debug.Log("No key at position : " + snpPos);
+            return Vector3.zero;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        if (DicCells != null)
+        {
+            foreach (Vector3 cellPos in DicCells.Keys)
+            {
+                Gizmos.DrawWireCube(cellPos + new Vector3(cellsSize / 2, cellsSize / 2, 0), Vector3.one * cellsSize);
+            }
+        }
+    }
+}
--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,48 +1,37 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
     [SerializeField] private GameObject roomPreviewPrefab = null;
-    [SerializeField] private float width = 0;
-    [SerializeField] private float height = 0;
 
     private GameObject roomPreviewGO;
     private Camera mainCamera;
-    private CapsuleCollider[] verticesColliders;
+    private Grid grid;
 
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = Camera.main;
+        grid = GetComponent<Grid>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(roomPreviewGO != null)
         {
-            roomPreviewGO = Instantiate(roomPreviewPrefab, GetMousePositionInWorld(), Quaternion.identity);
-            verticesColliders = roomPreviewGO.GetComponents<CapsuleCollider>();
-        }
-
-        if (roomPreviewGO != null)
-        {
-            roomPreviewGO.transform.position = GetMousePositionInWorld();
-            CheckSnapping();
+            roomPreviewGO.transform.position = grid.SnapToGrid(GetMousePositionInWorld());
         }
     }
 
     Vector3 GetMousePositionInWorld()
     {
-        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
-        Input.mousePosition.y, -mainCamera.gameObject.transform.position.z));
-        return mousePosition;
+        return mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -mainCamera.transform.position.z));
     }
 
-    void CheckSnapping()
+    public void CreateRoom()
     {
+        roomPreviewGO = Instantiate(roomPreviewPrefab, GetMousePositionInWorld(), Quaternion.identity);
     }
 }
