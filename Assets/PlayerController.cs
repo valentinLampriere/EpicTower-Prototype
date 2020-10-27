c8bf9acb971eaedac755537eda211452773f1d24
Valentin Lamprière <valentin.lampriere1@gmail.com>
2020-10-27 19:28:19 +0100

    :airplane:  Changing ladder and room to fit with player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    private float speed = 8f;
+    Rigidbody rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        float x = Input.GetAxis("Horizontal");
+        rb.velocity += Vector3.right * x * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        
+    }
+}
