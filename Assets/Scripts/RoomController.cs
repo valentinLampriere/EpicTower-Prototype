8360fa94eac7fa73b97d6a418523504629c55d37
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-10-27 15:26:44 +0100

    create a room on mouse position and collide with neighbours rooms vertices (wip)

---
+++ Assets/Scripts/RoomController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomController : MonoBehaviour
+{
+    [SerializeField] private GameObject roomPreviewPrefab = null;
+    [SerializeField] private float width = 0;
+    [SerializeField] private float height = 0;
+
+    private GameObject roomPreviewGO;
+    private Camera mainCamera;
+    private CapsuleCollider[] verticesColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            roomPreviewGO = Instantiate(roomPreviewPrefab, GetMousePositionInWorld(), Quaternion.identity);
+            verticesColliders = roomPreviewGO.GetComponents<CapsuleCollider>();
+        }
+
+        if (roomPreviewGO != null)
+        {
+            roomPreviewGO.transform.position = GetMousePositionInWorld();
+            CheckSnapping();
+        }
+    }
+
+    Vector3 GetMousePositionInWorld()
+    {
+        Vector3 mousePosition = mainCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+        Input.mousePosition.y, -mainCamera.gameObject.transform.position.z));
+        return mousePosition;
+    }
+
+    void CheckSnapping()
+    {
+    }
+}
---
+++ Assets/Scripts/VerticeCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticeCollider : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetType().IsEquivalentTo(typeof(CapsuleCollider)))
+        {
+            Debug.Log((CapsuleCollider)other);
+        }
+    }
+}
