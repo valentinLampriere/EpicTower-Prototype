d3634fc0e64f5d012f2c111be092ce992a2e71e1
HarmonySeeker <43490370+HarmonySeeker@users.noreply.github.com>
2020-10-29 17:03:27 +0100

    updated room prefab

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -8,6 +8,7 @@ public class Room : MonoBehaviour
     public float Height;
     public float Thickness;
     public bool IdleTrapPlaced = false;
+    public Transform BottomBlock;
 
     public Vector3 CenterPosition { get; set; }
     public List<Room> NeigbourRooms { get; set; }
@@ -24,8 +25,7 @@ public class Room : MonoBehaviour
         Vector3 TrapPosition = transform.position;
         
         IdleTrapPlaced = true;
-        TrapPosition.y -= Height / 2f;
-        TrapPosition.y += 0.5f + trap.transform.localScale.y / 2f;
+        TrapPosition.y -= Height / 2f - BottomBlock.localScale.y - trap.transform.localScale.y / 2f;
         TrapPosition.z += Thickness / 4;
         PlacedTrap = Instantiate(trap, TrapPosition, Quaternion.identity, transform);
     }
