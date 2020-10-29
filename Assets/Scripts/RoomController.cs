ac943363dabbb9c5079789b1c004eb7c44e84838
Valentin CLIMPONT <valentin.climpont@gmail.com>
2020-10-29 12:19:18 +0100

    created ladders to link a room to its horizontal neighbours

--- Assets/Scripts/RoomController.cs
+++ Assets/Scripts/RoomController.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RoomController : MonoBehaviour
 {
     [SerializeField] private GameObject roomPreviewPrefab = null;
+    [SerializeField] private GameObject ladderPrefab = null;
+    [SerializeField] private GameObject doorPrefab = null;
 
     private GameObject roomPreviewGO;
     private Camera mainCamera;
@@ -32,7 +35,73 @@ public class RoomController : MonoBehaviour
 
     void LinkToNeighbourRooms(Room room)
     {
+        foreach (Room neighRoom in room.NeigbourRooms)
+        {
+            int relPos = NeighbourRelativePosition(room, neighRoom);
 
+            // If is horizontal neighbour
+            if (relPos == 0)
+            {
+                // Create door and stairs
+                if(room.CenterPosition.x < neighRoom.CenterPosition.x)
+                {
+                    CreateLadder(
+                        new Vector3(room.CenterPosition.x + (room.Width / 2) - 1, room.CenterPosition.y - (room.Height / 2) + 1, 0),
+                        new Vector3(neighRoom.CenterPosition.x - (neighRoom.Width / 2) + 1, neighRoom.CenterPosition.y - (neighRoom.Height / 2) + 1, 0));
+                }
+                else
+                {
+                    CreateLadder(
+                        new Vector3(room.CenterPosition.x - (room.Width / 2) + 1, room.CenterPosition.y - (room.Height / 2) + 1, 0),
+                        new Vector3(neighRoom.CenterPosition.x + (neighRoom.Width / 2) - 1, neighRoom.CenterPosition.y - (neighRoom.Height / 2) + 1, 0));
+                }
+            }
+            // If is vertical neighbour
+            else if(relPos == 1)
+            {
+                // Find a way from the neighbour to the current room with max distance
+
+            }
+        }
+    }
+
+    void CreateLadder(Vector3 roomAnchor, Vector3 neighRoomAnchor)
+    {
+        Vector3 ladderPos = roomAnchor + neighRoomAnchor;
+        ladderPos = new Vector3(ladderPos.x / 2, ladderPos.y / 2, 0);
+
+        float angle = Mathf.Atan2(neighRoomAnchor.y - roomAnchor.y, neighRoomAnchor.x - roomAnchor.x);
+        angle = Mathf.Rad2Deg * angle;
+
+        float dist = (roomAnchor - neighRoomAnchor).magnitude;
+
+        GameObject ladderGO = Instantiate(ladderPrefab, ladderPos, Quaternion.identity);
+        ladderGO.transform.localScale = new Vector3(ladderGO.transform.localScale.x, dist, ladderGO.transform.localScale.z);
+        NavMeshLink links = ladderGO.GetComponentInChildren<NavMeshLink>();
+        links.startPoint = new Vector3(0, -dist / 2, 0);
+        links.endPoint = new Vector3(0, dist / 2, 0);
+        ladderGO.transform.rotation = Quaternion.Euler(0, 0, angle + 90f);
+    }
+
+    // returns 0 if horizontal neighbour, 1 if vertical
+    int NeighbourRelativePosition(Room room, Room neighRoom)
+    {
+        Vector3 neighRoomFirstVertice = neighRoom.CenterPosition - new Vector3(neighRoom.Width / 2, neighRoom.Height / 2, 0);
+        Vector3 neighRoomLastVertice = neighRoom.CenterPosition + new Vector3(neighRoom.Width / 2, neighRoom.Height / 2, 0);
+
+        if (neighRoomFirstVertice.x == room.CenterPosition.x + (room.Width / 2) || neighRoomLastVertice.x == room.CenterPosition.x - (room.Width / 2))
+        {
+            Debug.Log(room + " " + neighRoom + " horizontal");
+            return 0;
+        }
+        else if(neighRoomFirstVertice.y == room.CenterPosition.y + (room.Height / 2) || neighRoomLastVertice.y == room.CenterPosition.y - (room.Height / 2))
+        {
+            Debug.Log(room + " " + neighRoom + " vertical");
+            return 1;
+        }
+
+        Debug.Log(room + " " + neighRoom + " PROBLEM");
+        return -1;
     }
 
     public void CreateRoom()
@@ -47,6 +116,7 @@ public class RoomController : MonoBehaviour
 
             grid.AddRoomInGrid(roomCenter, room);
             room.NeigbourRooms = grid.GetNeighbourRooms(room);
+            LinkToNeighbourRooms(room);
         }
     }
 }
